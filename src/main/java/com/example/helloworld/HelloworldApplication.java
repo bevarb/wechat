b13a76610b15bd7f7a70f34d035c848b367a866c
bevarb <775374937@qq.com>
2023-02-26 22:39:55 +0800

    a

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -4,22 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.apache.log4j.Logger;
 // import java.lang.Thread;
 
 @SpringBootApplication
 @Controller
-@GetMapping("/cat/message")
+@RequestMapping("/cat/message")
 public class HelloworldApplication {
 
     @RestController
     class HelloworldController {
-        @GetMapping(value = "security", method = RequestMethod.GET)
+        @RequestMapping(value = "security", method = RequestMethod.GET)
         public void doGet() {
             System.out.println("这是get方法！");
         }
 
-        @GetMapping(value = "security", method = RequestMethod.POST)
+        @RequestMapping(value = "security", method = RequestMethod.POST)
         // post方法用于接收微信服务端消息
         public void DoPost() {
             System.out.println("这是post方法！");
